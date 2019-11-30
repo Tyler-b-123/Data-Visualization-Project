6492447cfa617f509d9222774484f129e10d730c
Tyler-b-123 <49565435+Tyler-b-123@users.noreply.github.com>
2019-11-29 13:03:53 -1200

    Add files via upload
    fixed the hover to make it go based of point not centered

--- golfStats.js
+++ golfStats.js
@@ -72,8 +72,8 @@ function stuff(data){
                 .duration('50')
                 .style("opacity", 1);
             div.html(d["Name"])
-                .style("left", (d3.event.pageX + 1) + "px")
-                .style("right", (d3.event.pageY + 1) + "px");
+                .style("left", (d3.event.pageX + 10) + "px")
+                .style("top", (d3.event.pageY - 15) + "px");
 
         })
         .on('mouseout', function (d,i){
